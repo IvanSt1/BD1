777991cc6c6b9aada9aa824dbbd271286c6fc857
IvanSt1 <70325530+IvanSt1@users.noreply.github.com>
2023-05-22 15:29:46 +0300

    Update school.sql

--- lab1-1/school.sql
+++ lab1-1/school.sql
@@ -1,26 +1,26 @@
 CREATE TABLE teachers (
-	teacher_id integer,
+	teacher_id integer PRIMARY KEY AUTOINCREMENT,
 	name text
 );
 
 CREATE TABLE subjects (
-	subject_id integer,
+	subject_id integer PRIMARY KEY AUTOINCREMENT,
 	name text
 );
 
 CREATE TABLE groups (
-	group_id integer,
+	group_id integer PRIMARY KEY AUTOINCREMENT,
 	name text
 );
 
 CREATE TABLE students (
-	student_id integer,
+	student_id integer PRIMARY KEY AUTOINCREMENT,
 	name text,
 	group_id integer
 );
 
 CREATE TABLE schedule (
-	schedule_id integer,
+	schedule_id integer PRIMARY KEY AUTOINCREMENT,
 	group_id integer,
 	subject_id integer,
 	teacher_id integer,
@@ -28,24 +28,24 @@ CREATE TABLE schedule (
 );
 
 CREATE TABLE grades (
-	student_id integer,
+	student_id integer PRIMARY KEY AUTOINCREMENT,
 	subject_id integer,
 	grade integer,
 	date date
 );
 
 CREATE TABLE parents (
-	parent_id integer,
+	parent_id integer PRIMARY KEY AUTOINCREMENT,
 	name text
 );
 
 CREATE TABLE studentparents (
-	student_id integer,
+	student_id integer PRIMARY KEY AUTOINCREMENT,
 	parent_id integer
 );
 
 CREATE TABLE attendance (
-	student_id integer,
+	student_id integer PRIMARY KEY AUTOINCREMENT,
 	date date,
 	status text
 );
